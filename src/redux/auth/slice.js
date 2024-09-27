2c1724ccb79a91c78f5b9b1bae5046353cd3042b
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-09-27 06:33:14 +0300

    login added

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,5 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { register } from "./operations";
+import { createSlice, isAnyOf } from "@reduxjs/toolkit";
+import { logIn, register } from "./operations";
 
 const authSlice = createSlice({
   name: "auth",
@@ -11,15 +11,33 @@ const authSlice = createSlice({
     token: null,
     isLoggedIn: false,
     isRefreshing: false,
+    isLoading: false,
+    isError: false,
   },
   extraReducers: (builder) => {
     console.log(builder);
 
-    builder.addCase(register.fulfilled, (state, action) => {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    });
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addMatcher(
+        isAnyOf(register.pending, logIn.pending),
+        (state) => (state.isLoading = true)
+      )
+      .addMatcher(
+        isAnyOf(register.rejected, logIn.rejected),
+        (state, action) => (
+          (state.isLoading = false), (state.isError = action.payload)
+        )
+      );
   },
 });
 export default authSlice.reducer;
